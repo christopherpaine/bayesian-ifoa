38acc5ca99a5c417a89d6dcfeb75757dd40c3840
Christopher J. Paine <64914582+christopherpaine@users.noreply.github.com>
2025-07-17 23:27:41 +0000

    .

--- get-kernel-id-in-progress.py
+++
@@ -1,7 +0,0 @@
-import os
-import ipykernel
-
-connection_file = os.path.basename(ipykernel.connect.get_connection_file())
-kernel_id = connection_file.split('-', 1)[1].split('.')[0]
-print(kernel_id)
-
