716bd15e77d88cfd686701811d09272e2a3267ef
installer <installer@gmail.com>
2025-07-18 14:17:06 +0100

    .

--- docs/copy-files-across.sh
+++ docs/copy-files-across.sh
@@ -1,5 +1,6 @@
 cp ~/123/bayesian_inference/chris_pca/lets-go-baby.html ./  
 cp ~/123/bayesian_inference/chris_pca/lets-go-baby-clean.html ./
+./update-time.sh
 git add .
 git commit -m "."
 git push
---
+++ docs/update-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Path to your HTML file
+HTML_FILE="index.html"
+
+# Get current date and time in desired format
+NEW_TIMESTAMP=$(date +"%A %dth %H:%M%P")
+
+# Use sed to replace the old timestamp
+sed -i -E "s|(last updated ).* </p>|\1$NEW_TIMESTAMP </p>|" "$HTML_FILE"
+
